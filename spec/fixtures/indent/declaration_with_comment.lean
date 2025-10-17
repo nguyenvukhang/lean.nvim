dbbf42053e0174141a9f7ff953b48cc466d11b7d
nguyenvukhang <brew4k@gmail.com>
2025-10-17 17:46:15 +0800

    Delete everything indent-related

--- spec/fixtures/indent/declaration_with_comment.lean
+++
@@ -1,8 +0,0 @@
-/-- foo
-  bar
-  baz -/
-theorem quux : 0 = 0 := by
-  have : 0 = 0 := rfl
-  -- some comment
-  have : 1 = 1 := rfl
-  rfl
--- spec/fixtures/indent/structure_notation.in.lean
+++
@@ -1,8 +0,0 @@
-structure Point where
-  x : Nat
-  y : Nat
-
-def p : Point := {
-x := 37
-y := 73
-}
